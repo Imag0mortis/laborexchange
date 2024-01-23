ce29620ddc41c1e65a9d2b1a24a8d77ad42bf713
Imag0mortis <117734513+Imag0mortis@users.noreply.github.com>
2024-01-23 19:54:13 +0300

    update

--- Desktop/Test-Task/backend/main.js
+++ Desktop/Test-Task/backend/main.js
@@ -8,19 +8,79 @@ var convert = require('xml-js');
 var xml = require('fs').readFileSync('./example_1.xml', 'utf8');
 
 var result = convert.xml2json(xml, {compact: true, spaces: 4});
-console.log(result);
 
+// FIXME (обработать отсутствие данных в списке)
+
+var vacanciesList = JSON.parse(result)["source"]["vacancies"]["vacancy"];
+console.log(vacanciesList);
 
 //Вот тут логика
+var page = 1;
+var pageSize = 10;
+var custom = "abcd";
+  
+if(vacanciesList == null || vacanciesList == undefined || vacanciesList == NaN) {
+  console.log("Список пуст");
+}
 
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader('Content-Type', 'application/json');
-  res.end(result);
-});
+ const server = http.createServer((req, res) => {
+   var page = 1;
+   var pageSize = 10;
+   res.statusCode = 200;
+   res.setHeader("Access-Control-Allow-Origin", "*");
+   res.setHeader('Content-Type', 'application/json');
+   res.end(result);
+ });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+ server.listen(port, hostname, () => {
+   console.log(`Server running at http://${hostname}:${port}/`);
+ });
+
+
+
+
+/*
+if (page.isNaN || page == null || page == undefined ) {
+  const server = http.createServer((req, res) => {
+
+    res.statusCode = 200;
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Content-Type', 'application/json');
+    res.end(vacanciesList);
+  });
+  
+  server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
+} else {
+  //var newVacanciesList = vacanciesList.slice(0, 1);
+  console.log(vacanciesList);
+  const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Content-Type', 'application/json');
+    res.end(vacanciesList);
+ });
+  
+  server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
+}
+
+*/
+
+// const server = http.createServer((req, res) => {
+
+//   var page = 1;
+//   var pageSize = 10;
+//   var custom = "abcd";
+//   res.statusCode = 200;
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader('Content-Type', 'application/json');
+//   res.end(result);
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
